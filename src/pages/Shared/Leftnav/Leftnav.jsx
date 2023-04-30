9553de733c63db1769eaec76b7976de67079b077
MDNirobkhan1 <nirob937612@gmail.com>
2023-04-30 12:17:18 +0600

    rating add

--- src/pages/Shared/Leftnav/Leftnav.jsx
+++ src/pages/Shared/Leftnav/Leftnav.jsx
@@ -10,7 +10,7 @@ const Leftnav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/categories')
+        fetch('https://the-news-dragon-server-mdnirobkhan1.vercel.app/categories')
             .then(res => res.json())
             .then(data => setCategories(data))
             .catch(error => console.error(error))
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -44,7 +44,7 @@ import Terms from "../pages/Shared/Terms/Terms";
             {
               path:':id',
               element:<Category></Category>,
-              loader:({params})=> fetch(`http://localhost:5000/categories/${params.id}`)
+              loader:({params})=> fetch(`https://the-news-dragon-server-mdnirobkhan1.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -55,7 +55,7 @@ import Terms from "../pages/Shared/Terms/Terms";
         {
           path:':id',
           element:<PrivateRoute><News></News></PrivateRoute>,
-          loader:({params})=> fetch(`http://localhost:5000/news/${params.id}`)
+          loader:({params})=> fetch(`https://the-news-dragon-server-mdnirobkhan1.vercel.app/news/${params.id}`)
         }
       ]
     }
